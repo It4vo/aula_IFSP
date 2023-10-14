ba688b2704e5643ddad1f1db47db32fc2ed42656
Rafael <135732971+It4vo@users.noreply.github.com>
2023-10-14 16:40:04 -0300

    Update media python.py

--- media python.py
+++ media python.py
@@ -3,20 +3,20 @@ ifa=0
 apr=0
 repr=0
 while(i<4):
+    nome=input("digite um nome:");
     P1=input("digite uma nota:");
     P2=input("digite uma nota:");
-
     m=float(P1)+float(P2);
     m=m/2;
     print(m);
     if(m>=6):
-        print("aprovado");           #tem que ter indentar para funcionar código
+        print(nome,"foi aprovado");           #tem que ter indentar para funcionar código
         apr=apr+1
     elif(m<6 and m>4):
-        print("em ifa");
+        print(nome,"está em ifa");
         ifa = ifa + 1
     else:
-        print("reprovado");
+        print(nome,"foi reprovado");
         repr=repr+1
     i=i+1
 
