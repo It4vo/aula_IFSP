143de70bd76e27db8fd9ddd6afbe4a837d3d12d6
Rafael <135732971+It4vo@users.noreply.github.com>
2023-11-01 14:35:57 -0300

    Add files via upload

---
+++ anotações.py
@@ -0,0 +1,6 @@
+num=[0,0,0,0,0,0,0,]     #lista
+x=(0,0,0,0,0,0,0,0,0,0,0,)      #tupla
+
+for i in range (10)     #vai de 0 até 9
+for i in range (2,10)     #vai de 2 até 9
+for i in range (2,10,3)     #vai de 2 até 9 de 3 em 3
---
+++ funçao.py
@@ -0,0 +1,13 @@
+
+
+def lerNotas(n1,n2):          #def serve para indicar funçao
+    n1=int(input("Digite n1:"))           
+    n2=int(input("Digite n2:"))
+
+def calculaMedia(n1,n2):
+    m1=n1+n2/2
+    return m1
+
+ler_notas(n1,n2)
+m=calcula_media(n1,n2)
+print(m)
