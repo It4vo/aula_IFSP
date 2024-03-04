8399b14cea1ec61c8fe51e8499d4a020241514d8
Rafael <135732971+It4vo@users.noreply.github.com>
2024-03-04 17:02:48 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from pessoa import Gente
-from carro  import Carros
+class Funcionarios:
+    def __init__(self, nome, prontuario, salario) :
+        self.nome=nome
+        self.prontuario=prontuario
+        self.salario=salario
+    def exibirInfo(self):
+        return f"Nome: {self.nome}, salário: R${self.salario:.2f}, Prontuario: {self.prontuario}"
+igor=Funcionarios("igor de Moraes", "SP3117821",501.64)
+print(igor.exibirInfo())
 
-p = Gente('Rafael', 16)
+class servidoradm(Funcionarios):
+    def __init__(self, nome, prontuario, salario, cargo):
+        super().__init__(nome, prontuario, salario)
+        self.cargo=cargo
 
-p.apresentar()
-
-carro = Carros(100, '2025', 'Rosa', 'BMW')
-
-carro.vrum_vrum()
+    def exibirInfo(self):
+        return super().exibirInfo()+ f",Cargo: {self.cargo}"
+claudio= servidoradm("Claudio Oreio","SP5678",2100,"coletor")
+print (claudio.exibirInfo())
